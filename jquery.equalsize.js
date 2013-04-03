09af0e0c648e7d87063ad93a779c82ca6e70e8d1
Gijs Jorissen <gizburdt@gmail.com>
2013-04-03 23:13:34 +0200

    Don't repeat yourself

--- jquery.equalsize.js
+++ jquery.equalsize.js
@@ -1,49 +1,62 @@
 /**
  * Jquery equalSize
+ * https://github.com/Gizburdt/jQuery-equalSize
+ * 
+ * @copyright Gijs Jorissen
+ * http://gizburdt.com
+ *
+ * With jQuery.equalSize it's possible to set the same width or height for elements
  */
 
 (function($) {
 
-	$.fn.equalWidth = function( options ) 
-	{
-		var defaults 	= {
-				min: null
-			},
-			options 	= $.extend( defaults, options ),
-			tallest		= options.min ? options.min : 0;
-
-		this.each(function()
-		{
-			var that 	= $(this);
-			if( that.width() > tallest ) tallest = that.width();
-		});
-
-		return this.each(function()
-		{
-			var that	= $(this);
-			that.css({ minWidth: tallest });
-		});
-	};
-
+	/**
+	 * Set the same height
+	 * 
+	 * @param  	array 	options
+	 * @return 	void
+	 */
 	$.fn.equalHeight = function( options ) 
 	{
-		var defaults 	= {
-				min: null
-			},
+		var defaults 	= { min: 0 },
 			options 	= $.extend( defaults, options ),
-			tallest		= options.min ? options.min : 0;
+			that		= this;
+		
+		return this.each(function(){ that.equalSize( options, 'height' ) });
+	}
 
-		this.each(function()
-		{
-			var that 	= $(this);
-			if( that.height() > tallest ) tallest = that.height();
-		});
+	/**
+	 * Set the same width
+	 * 
+	 * @param  	array 	options
+	 * @return 	void
+	 */
+	$.fn.equalWidth = function( options ) 
+	{
+		var defaults 	= { min: 0 },
+			options 	= $.extend( defaults, options ),
+			that		= this;
+		
+		return this.each(function(){ that.equalSize( options, 'width' ) });
+	}
 
-		return this.each(function()
-		{
-			var that	= $(this);
-			that.css({ minHeight: tallest });
-		});
-	};
+	/**
+	 * Set a dimension (height, width) to the same value
+	 * 
+	 * @param  	array 	options
+	 * @param 	string 	dimension
+	 * @return 	void
+	 */
+	$.fn.equalSize = function( options, dimension )
+	{
+		var defaults	= {	min: 0 },
+			options		= $.extend( defaults, options ),
+			that		= $(this),
+			tallest		= options.min,
+			tallest		= (dimension == 'height' ) ? ( that.height() > tallest ? that.height() : tallest ) : ( that.width() > tallest ? that.width() : tallest ),
+			properties	= (dimension == 'height' ) ? { minHeight: tallest } : { minWidth: tallest };
+			
+		that.css( properties );
+	}
 
 })(jQuery);
